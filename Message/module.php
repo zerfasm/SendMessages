3f789a25cb6e9271d07ecb8ecaeb70d2e473ce7a
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-11-16 23:01:22 +0100

    Update module.php

--- Message/module.php
+++ Message/module.php
@@ -52,8 +52,6 @@ class SendMessages extends IPSModule
     {
         //Never delete this line!
         parent::ApplyChanges();
-	$triggerID = $this->ReadPropertyInteger("Ausloeser");
-	$this->RegisterMessage($triggerID, VM_UPDATE);
     }
 
     public function Update()
